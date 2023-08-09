e825aaff528b478085b2902b07867f8154139108
Lesly Samaritano Ayala <1628112@utp.edu.pe>
2023-08-09 17:26:12 -0500

    remove ruta in appcupertino/lib/presentation/screen/screens.dart

--- appcupertino/lib/presentation/screen/screens.dart
+++ appcupertino/lib/presentation/screen/screens.dart
@@ -1,5 +1,4 @@
 export 'package:appcupertino/presentation/screen/buttons/buttons_screen.dart';
-
 export 'package:appcupertino/presentation/screen/cards/cards_screen.dart';
 export 'package:appcupertino/presentation/auth/register_screen.dart';
 export 'package:appcupertino/presentation/screen/splash/loading_screen.dart';
