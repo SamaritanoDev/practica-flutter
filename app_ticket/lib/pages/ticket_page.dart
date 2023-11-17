e22fbf1069f83f175282f5f6382a4f44f5c581be
Flutterina Mobile <91768959+SamaritanoDev@users.noreply.github.com>
2023-11-17 01:16:59 -0500

    add take ticketPage

--- app_ticket/lib/pages/ticket_page.dart
+++ app_ticket/lib/pages/ticket_page.dart
@@ -2,6 +2,7 @@ import 'package:app_ticket/pages/blocs/ticket/ticket_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
 
 class TicketPage extends StatelessWidget {
   final TicketState ticketState;
@@ -11,54 +12,95 @@ class TicketPage extends StatelessWidget {
     required this.ticketState,
   });
 
+  String formatDateTime(DateTime dateTime) {
+    // Utiliza el formato deseado, en este caso, 'dd/MM/yyyy'
+    final formatter = DateFormat('dd/MM/yyyy');
+    return formatter.format(dateTime);
+  }
+
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
     if (ticketState is TicketGenerated) {
       final generatedState = ticketState as TicketGenerated;
+      final readTicketCubit = context.read<TicketCubit>();
+      final GlobalKey globalKey = GlobalKey();
 
       return BlocBuilder<TicketCubit, TicketState>(
         builder: (context, state) {
           return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(
-                    'assets/logo.svg',
-                    color: const Color(0xff602E9E),
-                    width: 250,
-                    height: 80,
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.purple,
+              onPressed: () => readTicketCubit.captureWidget(globalKey),
+              child: const Icon(
+                Icons.save,
+                color: Colors.white,
+              ),
+            ),
+            body: RepaintBoundary(
+              key: globalKey,
+              child: Center(
+                child: Container(
+                  padding: const EdgeInsets.all(5),
+                  height: 500,
+                  width: 350,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: const Color(0xff602E9E),
+                    ),
                   ),
-                  const SizedBox(height: 50),
-                  const TextRow(
-                    emoji: '👩‍💻',
-                    prefix: 'Contractor',
-                    value: 'Lesly Samaritano',
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/logo.svg',
+                        color: const Color(0xff602E9E),
+                        width: 250,
+                        height: 80,
+                      ),
+                      const SizedBox(height: 20),
+                      const Divider(
+                        height: 2,
+                        color: Color(0xff602E9E),
+                      ),
+                      const SizedBox(height: 50),
+                      const TextRow(
+                        emoji: '👩‍💻',
+                        prefix: 'Contractor',
+                        value: 'Lesly Samaritano',
+                      ),
+                      const SizedBox(height: 10),
+                      TextRow(
+                        emoji: '👩',
+                        prefix: 'Name client',
+                        value: generatedState.clientName,
+                      ),
+                      const SizedBox(height: 10),
+                      TextRow(
+                        emoji: '🗓️',
+                        prefix: 'Date of issue',
+                        value: formatDateTime(
+                            DateTime.parse(generatedState.dateOfIssue)),
+                      ),
+                      const SizedBox(height: 10),
+                      TextRow(
+                        emoji: '⚙️',
+                        prefix: 'Service',
+                        value: generatedState.selectedService.name,
+                      ),
+                      const SizedBox(height: 10),
+                      TextRow(
+                        emoji: '💰',
+                        prefix: 'Total Amount',
+                        value: '\$${generatedState.selectedService.price}',
+                      ),
+                      const SizedBox(height: 50),
+                      Text('Thanks for your preference 🥹💜',
+                          style: textTheme.titleLarge)
+                    ],
                   ),
-                  const SizedBox(height: 10),
-                  TextRow(
-                    emoji: '🗓️',
-                    prefix: 'Date of issue',
-                    value: generatedState.dateOfIssue,
-                  ),
-                  const SizedBox(height: 10),
-                  TextRow(
-                    emoji: '⚙️',
-                    prefix: 'Service',
-                    value: generatedState.selectedService.name,
-                  ),
-                  const SizedBox(height: 10),
-                  TextRow(
-                    emoji: '💰',
-                    prefix: 'Total Amount',
-                    value: '\$${generatedState.selectedService.price}',
-                  ),
-                  const SizedBox(height: 50),
-                  Text('Thanks for your preference 🥹💜',
-                      style: textTheme.headlineSmall)
-                ],
+                ),
               ),
             ),
           );
